eed67277bbad06f6e452fbd93af71333df31d74c
Juan Manuel Rodriguez <juan.m.rodriguez22@gmail.com>
2019-06-30 11:52:26 -0300

    User details complete

--- app/src/main/java/com/me/squad/intivefdvapp/userdetails/DetailsFragment.java
+++ app/src/main/java/com/me/squad/intivefdvapp/userdetails/DetailsFragment.java
@@ -7,10 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.me.squad.intivefdvapp.R;
 import com.me.squad.intivefdvapp.model.User;
+import com.squareup.picasso.Picasso;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -27,10 +29,28 @@ public class DetailsFragment extends Fragment implements DetailsContract.View {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_details, container, false);
+
+        // Get bundle data
         Bundle bundle = getArguments();
         User user = (User) bundle.getSerializable("selected");
-        TextView textView = root.findViewById(R.id.test_text);
-        textView.setText(user.getName().getFirst());
+
+        // Fill user data
+        TextView usernameView = root.findViewById(R.id.username);
+        usernameView.setText(String.format("%s %s", getString(R.string.username_label), user.getLogin().getUsername()));
+        TextView firstNameView = root.findViewById(R.id.user_first_name);
+        firstNameView.setText(String.format("%s %s", getString(R.string.first_name_label), user.getName().getFirst()));
+        TextView lastNameView = root.findViewById(R.id.user_last_name);
+        lastNameView.setText(String.format("%s %s", getString(R.string.last_name_label), user.getName().getLast()));
+        TextView emailView = root.findViewById(R.id.user_email);
+        emailView.setText(String.format("%s %s", getString(R.string.email_label), user.getEmail()));
+        ImageView imageView = root.findViewById(R.id.user_image);
+        Picasso.get()
+                .load(user.getPicture().getLarge())
+                .placeholder(R.drawable.image_placeholder)
+                .error(R.drawable.no_image_available)
+                .fit()
+                .into(imageView);
+
         return root;
     }
 
